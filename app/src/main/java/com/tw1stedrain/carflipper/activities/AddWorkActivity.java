f5565598a69ebbb9d029ec597280799a3df134f4
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2019-07-03 17:55:40 -0700

    setup to add work is there, need to finish. work now has it's own model

--- app/src/main/java/com/tw1stedrain/carflipper/activities/AddWorkActivity.java
+++ app/src/main/java/com/tw1stedrain/carflipper/activities/AddWorkActivity.java
@@ -1,19 +1,64 @@
 package com.tw1stedrain.carflipper.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.tw1stedrain.carflipper.R;
+import com.tw1stedrain.carflipper.models.Work;
 
 public class AddWorkActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+
+    EditText addName;
+    EditText addTime;
+    EditText addDetails;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_work);
+
+        db = FirebaseFirestore.getInstance();
+
+    }
+
+    public void addWorkClick(View view) {
+        addName = findViewById(R.id.add_work_name);
+        addTime = findViewById(R.id.add_work_time);
+        addName = findViewById(R.id.add_work_details);
+
+        Work work = new Work();
+        work.setName(addName.getText().toString());
+        work.setTime(Long.parseLong(addTime.getText().toString()));
+        work.setDetails(addDetails.getText().toString());
+
+        db.collection("work")
+                .add(work)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("WORK", "added ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("WORK", "failed to save to database");
+                    }
+                });
     }
 
     public void goHome(View view) {
---
+++ app/src/main/java/com/tw1stedrain/carflipper/models/Work.java
@@ -0,0 +1,61 @@
+package com.tw1stedrain.carflipper.models;
+
+
+import com.google.firebase.firestore.Exclude;
+import com.google.firebase.firestore.IgnoreExtraProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@IgnoreExtraProperties
+public class Work {
+
+    @Exclude
+    private String id;
+
+    private String name;
+    private long time;
+    private String details;
+
+    private List<Part> parts = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public List<Part> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<Part> parts) {
+        this.parts = parts;
+    }
+}
